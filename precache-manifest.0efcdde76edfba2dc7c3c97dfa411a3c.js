b36b7f0d9b1ea4765ef8ee67da8e7369f98b9afd
Ignatzz <martinsandeen@gmail.com>
2019-04-02 20:01:55 -0400

    Updates

---
+++ precache-manifest.0efcdde76edfba2dc7c3c97dfa411a3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "85e974ae01bc6fd97695",
+    "url": "/clicky-game/static/js/main.ac4a4cfb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "85e974ae01bc6fd97695",
+    "url": "/clicky-game/static/css/main.7c1a5f71.chunk.css"
+  },
+  {
+    "revision": "93db8e13724dd1a27b4ba21f7a6fd476",
+    "url": "/clicky-game/index.html"
+  }
+];
